ef6c1995961f031c7e7923eb47e47365f1f6fbed
Antoine Busque <antoinebusque@gmail.com>
2012-12-07 02:08:25 -0500

    Changed shit in main class

--- zombiefactory/zombiefactory/ZombieGame.cs
+++ zombiefactory/zombiefactory/ZombieGame.cs
@@ -11,18 +11,16 @@ using Microsoft.Xna.Framework.Media;
 
 namespace zombiefactory
 {
-    /// <summary>
-    /// This is the main type for your game
-    /// </summary>
     public class ZombieGame : Microsoft.Xna.Framework.Game
     {
+        const float FPS_INTERVAL = 1.0f;
+
         #region properties
         public GraphicsDeviceManager graphics;
         public SpriteBatch spriteBatch;
         public InputManager InputMgr { get; private set; }
         public ResourceManager<Texture2D> TextureMgr { get; private set; }
         public ResourceManager<SpriteFont> FontMgr { get; private set; }
-        public ResourceManager<Model> ModelMgr { get; private set; }
         public ResourceManager<Song> MusicMgr { get; private set; }
         public ResourceManager<SoundEffect> SfxMgr { get; private set; }
         public FpsCounter FpsHandler { get; private set; }
@@ -32,25 +30,27 @@ namespace zombiefactory
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
+            graphics.SynchronizeWithVerticalRetrace = true;
+            IsFixedTimeStep = true;
+            IsMouseVisible = false;
         }
 
-        /// <summary>
-        /// Allows the game to perform any initialization it needs to before starting to run.
-        /// This is where it can query for any required services and load any non-graphic
-        /// related content.  Calling base.Initialize will enumerate through any components
-        /// and initialize them as well.
-        /// </summary>
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
+            //Resource Managers
+            FontMgr = new ResourceManager<SpriteFont>(this);
+            TextureMgr = new ResourceManager<Texture2D>(this);
+            MusicMgr = new ResourceManager<Song>(this);
+            SfxMgr = new ResourceManager<SoundEffect>(this);
+
+            FpsHandler = new FpsCounter(this, FPS_INTERVAL);
+            InputMgr = new InputManager(this);
+            Components.Add(FpsHandler);
+            Components.Add(InputMgr);
 
             base.Initialize();
         }
 
-        /// <summary>
-        /// LoadContent will be called once per game and is the place to load
-        /// all of your content.
-        /// </summary>
         protected override void LoadContent()
         {
             // Create a new SpriteBatch, which can be used to draw textures.
@@ -59,24 +59,15 @@ namespace zombiefactory
             // TODO: use this.Content to load your game content here
         }
 
-        /// <summary>
-        /// UnloadContent will be called once per game and is the place to unload
-        /// all content.
-        /// </summary>
         protected override void UnloadContent()
         {
             // TODO: Unload any non ContentManager content here
         }
 
-        /// <summary>
-        /// Allows the game to run logic such as updating the world,
-        /// checking for collisions, gathering input, and playing audio.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
             // Allows the game to exit
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+            if (InputMgr.ControllerState.Buttons.Back == ButtonState.Pressed)
                 this.Exit();
 
             // TODO: Add your update logic here
@@ -84,10 +75,6 @@ namespace zombiefactory
             base.Update(gameTime);
         }
 
-        /// <summary>
-        /// This is called when the game should draw itself.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
