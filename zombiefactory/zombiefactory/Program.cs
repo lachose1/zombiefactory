149ce211134bb2cbbbae75c436e7e804f9c1dc3f
Antoine Busque <antoinebusque@gmail.com>
2012-12-07 00:55:16 -0500

    Changed main class name/filename

--- zombiefactory/zombiefactory/Program.cs
+++ zombiefactory/zombiefactory/Program.cs
@@ -10,7 +10,7 @@ namespace zombiefactory
         /// </summary>
         static void Main(string[] args)
         {
-            using (Game1 game = new Game1())
+            using (ZombieFactory game = new ZombieFactory())
             {
                 game.Run();
             }
