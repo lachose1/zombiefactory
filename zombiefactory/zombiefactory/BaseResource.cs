14263a3693df7038b0e3802d30ff2927740f4617
Antoine Busque <antoinebusque@gmail.com>
2012-12-08 02:01:05 -0500

    Removed unused usings

--- zombiefactory/zombiefactory/BaseResource.cs
+++ zombiefactory/zombiefactory/BaseResource.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using Microsoft.Xna.Framework.Content;
-using Microsoft.Xna.Framework.Graphics;
 
 namespace zombiefactory
 {
--- zombiefactory/zombiefactory/InputManager.cs
+++ zombiefactory/zombiefactory/InputManager.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
 
-
 namespace zombiefactory
 {
     public class InputManager : Microsoft.Xna.Framework.GameComponent
