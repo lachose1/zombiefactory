cf9088692c62043072a70ab8138ff317469599e6
lachose1 <hugolaliberte@gmail.com>
2012-12-19 12:34:49 -0500

    Oubli de add

---
+++ zombiefactory/zombiefactory/Emitter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace zombiefactory
+{
+    class Emitter
+    {
+        ZombieGame ZombieGame { get; set; }
+        public int MaxParticles { get; set; }
+        public bool AutomaticSpawn { get; set; }
+        float NextSpawnCountdown { get; set; }
+        float TimeSinceLastSpawn { get; set; }
+        LinkedList<Particle> ActiveParticles;
+        Random Noise { get; set; }
+
+        public Emitter(ZombieGame game, string fileName, int frames, 
+            int lines, Vector2 position, Vector2 direction, float life,
+            int maxParticles, bool automaticSpawn)
+        {
+            ActiveParticles = new LinkedList<Particle>();
+        }
+    }
+}
