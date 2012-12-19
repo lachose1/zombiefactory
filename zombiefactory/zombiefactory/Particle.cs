a80ddd78c11b65f1909b89d9e2b3ed2435c3cfa3
lachose1 <hugolaliberte@gmail.com>
2012-12-19 12:14:02 -0500

    Ajout de la classe Particle

---
+++ zombiefactory/zombiefactory/Particle.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace zombiefactory
+{
+    class Particle : AnimatedSprite
+    {
+        public Vector2 Direction { get; set; }
+        public float Life { get; set; }
+
+        public Particle(ZombieGame game, string fileName, int frames, int lines, Vector2 position, Vector2 direction, float life)
+            : base(game, fileName, frames, lines, position)
+        {
+            Direction = direction;
+            Life = life;
+        }
+
+        public override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        public override bool Update(GameTime gameTime, float dt)
+        {
+            this.Position += this.Direction * dt;
+            this.Life -= dt;
+
+            base.Update(gameTime);
+
+            if (this.Life > 0)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
