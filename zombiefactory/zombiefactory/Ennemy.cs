a4d1556f546d0f367f6d59f7ae78f7facdac86bb
lachose1 <hugolaliberte@gmail.com>
2013-03-04 20:59:35 -0500

    ajout de quelques lignes plutot pointless

--- zombiefactory/zombiefactory/Ennemy.cs
+++ zombiefactory/zombiefactory/Ennemy.cs
@@ -9,13 +9,18 @@ namespace zombiefactory
         public const string SPRITE_NAME = "Link";
         public const int SPRITE_FRAMES = 3;
         public const int SPRITE_LINES = 4;
-        public const float MAX_SPEED = 150.0f;
         public const float DEPTH = 0.1f;
         #endregion constants
 
-        public Ennemy(ZombieGame game, Vector2 initPos)
+        #region properties
+        public float MaxSpeed { get; protected set; }
+        public bool IsMoving { get; protected set; }
+        #endregion properties
+
+        public Ennemy(ZombieGame game, Vector2 initPos, float maxSpeed)
             : base(game, SPRITE_NAME, SPRITE_FRAMES, SPRITE_LINES, initPos, DEPTH)
         {
+            MaxSpeed = maxSpeed;
         }
 
         public override void Initialize()
@@ -38,12 +43,45 @@ namespace zombiefactory
 
         private void SetSpriteDirection()
         {
+            //Run AI code for direction
+            float PlayerX = ZombieGame.Player.Sprite.Position.X;
+            float PlayerY = ZombieGame.Player.Sprite.Position.Y;
+
+            if (PlayerY > Sprite.Position.Y)
+            {
+                if (PlayerY > Math.Abs(PlayerX))
+                    Sprite.CurLine = (int)Direction.Up;
+                if (PlayerX < Sprite.Position.X)
+                    Sprite.CurLine = (int)Direction.Left;
+                else
+                    Sprite.CurLine = (int)Direction.Right;
+            }
+            else
+            {
+                if (PlayerX > Math.Abs(PlayerY))
+                    Sprite.CurLine = (int)Direction.Up;
+                if (PlayerX < Sprite.Position.X)
+                    Sprite.CurLine = (int)Direction.Left;
+                else
+                    Sprite.CurLine = (int)Direction.Right;
+            }
 
         }
 
         protected override void MoveSprite()
         {
+            //Run AI code for movement
+            float x = Sprite.Position.X;
+            float y = Sprite.Position.Y;
+            int Direction = Sprite.CurLine;
 
+            float speedX = ZombieGame.InputMgr.ControllerState.ThumbSticks.Left.X * MaxSpeed;
+            float speedY = ZombieGame.InputMgr.ControllerState.ThumbSticks.Left.Y * MaxSpeed;
+
+            Speed = new Vector2(speedX, speedY);
+
+            x += Speed.X / ZombieGame.FpsHandler.FpsValue;
+            y -= Speed.Y / ZombieGame.FpsHandler.FpsValue;
         }
 
         protected override bool IsCollision(float x, float y)
